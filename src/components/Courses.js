8dbcd7f2b0de201ed4eaa52199ef2467f4c1b2c6
Phillip Lackey <plackey@str.com>
2020-03-30 20:19:56 -0500

    Added a call to courses api and a Courses page with table of courses.  Also added basic navigation

---
+++ src/components/Courses.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { getCourses } from '../api/courseApi';
+
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    getCourses().then(courses => {
+      setCourses(courses);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Authors ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map(course => {
+            return (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default CoursesPage;
