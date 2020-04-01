7bd5bd43f9ed774b010873d8df7144350eeee410
Phillip Lackey <plackey@str.com>
2020-04-01 12:12:04 -0500

    Added flexible change event handler to the form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Prompt } from 'react-router-dom';
+import CourseForm from './CourseForm';
 
 const ManageCoursePage = props => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: '',
+    title: '',
+    authorId: null,
+    category: ''
+  });
+
+  function handleChange({ target }) {
+    setCourse({
+      ...course,
+      [target.name]: target.value
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      {props.match.params.slug}
+      <CourseForm course={course} onChange={handleChange} />
     </>
   );
 };
